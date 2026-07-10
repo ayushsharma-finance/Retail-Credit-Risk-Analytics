63cc00198b49a512fe525b1edce61ded2ec0b43e
Ayush Sharma <bhnausharma6147@gmail.com>
2026-07-10 22:22:47 +0530

    Add Branches table

--- sql/04_Branches.sql
+++ sql/04_Branches.sql
@@ -1,1 +1,28 @@
+USE projectSHIELD;
+GO
+CREATE TABLE BRANCHES
+(
+BRANCH_ID VARCHAR(15) PRIMARY KEY,
+BRANCH_CODE VARCHAR(10) NOT NULL UNIQUE,
+BRANCH_NAME VARCHAR(100) NOT NULL,
+IFSC_CODE VARCHAR(11) NOT NULL UNIQUE,
+BRANC_ADDRESS VARCHAR(255) NOT NULL,
+City               VARCHAR(50)     NOT NULL,
 
+    District           VARCHAR(50)     NOT NULL,
+
+    State              VARCHAR(50)     NOT NULL,
+
+    PIN_Code           VARCHAR(6)      NOT NULL,
+
+    Branch_Manager     VARCHAR(100)    NOT NULL,
+
+    Contact_Number     VARCHAR(15)     NOT NULL,
+
+    Email_ID           VARCHAR(100),
+
+    Branch_Status      VARCHAR(20)     DEFAULT 'Active',
+
+    Created_Date       DATETIME        DEFAULT GETDATE()
+);
+GO
